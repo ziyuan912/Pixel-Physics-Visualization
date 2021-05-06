72de05022f0b86537da3677dd15e9531e26d8994
ziyuan <superboy753@yahoo.com.tw>
2021-05-07 01:36:16 +0800

    update 3d aug

--- src/tolibx264.sh
+++ src/tolibx264.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-for dir in ./$1/*; do
-	echo $dir
-	for filename in $dir/*.mp4; do
-		mkdir ./test/${dir:18}
-		echo $filename
-	    ffmpeg -i $filename -vcodec libx264 -pix_fmt yuv420p ./test/${filename:18}
-	done
+for filename in $1/*.mp4; do
+	echo $filename
+    ffmpeg -i $filename -vcodec libx264 -pix_fmt yuv420p ./outuputs/${filename:23}
 done
